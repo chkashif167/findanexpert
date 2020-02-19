6fea63aa4783788728d98cca3d35cb6ca8ba8a9e
kashifdeltashoppe167 <kashif@deltahsoppe.com>
2020-02-19 11:29:06 +0500

    register form submission

--- ClientApp/src/components/BecomeExpert/ProviderAuthentication.js
+++ ClientApp/src/components/BecomeExpert/ProviderAuthentication.js
@@ -1,71 +1,110 @@
-﻿import React, { Component } from 'react';
-import { AuthenticateServiceProvider } from '../AuthenticateServiceProvider';
-import { RegisterServiceProvider } from '../RegisterServiceProvider';
-import { Redirect } from 'react-router-dom';
+﻿import React, { Component } from "react";
+import { AuthenticateServiceProvider } from "../AuthenticateServiceProvider";
+import { RegisterServiceProvider } from "../RegisterServiceProvider";
+import { Redirect } from "react-router-dom";
 
 export class ProviderAuthentication extends Component {
-    displayName = ProviderAuthentication.name
+  displayName = ProviderAuthentication.name;
 
-    state = {
-        redirect: false
+  state = {
+    redirect: false,
+    activeTab: "signIn"
+  };
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
+  };
+  renderRedirect = () => {
+    if (localStorage.getItem("serviceproviderid")) {
+      return <Redirect to="/provider-profile" />;
     }
-    setRedirect = () => {
-        this.setState({
-            redirect: true
-        })
+  };
+  handleActiveTabChange = activeTab => {
+    this.setState({
+      activeTab
+    });
+  };
+  render() {
+    document.getElementsByTagName("META")[2].content =
+      "Find an expert provides a fair opportunity to beauticians, handyman, IT personnel, and many others to register on for free and earn money online without any investment.";
+    document.getElementsByTagName("TITLE")[0].text =
+      "Provider Login or Create an Account | Find an Expert";
+
+    if (localStorage.getItem("serviceproviderid") != null) {
     }
-    renderRedirect = () => {
-        if (localStorage.getItem('serviceproviderid')) {
-            return <Redirect to='/provider-profile' />
-        }
-    }
-    render() {
+    const { activeTab } = this.state;
 
-        document.getElementsByTagName("META")[2].content = 'Find an expert provides a fair opportunity to beauticians, handyman, IT personnel, and many others to register on for free and earn money online without any investment.';
-        document.getElementsByTagName("TITLE")[0].text = 'Provider Login or Create an Account | Find an Expert';
+    return (
+      <div id="MainPageWrapper">
+        {this.renderRedirect()}
+        <section className="section-padding">
+          <div className="container">
+            <div className="row pb-4 mb-5 pt-5">
+              <div className="col-md-12 signinRegisterWrap mt-5 mb-5">
+                <ul
+                  class="nav nav-tabs nav-justified signinRegister"
+                  role="tablist"
+                >
+                  <li class="nav-item">
+                    <a
+                      class={`nav-link ${activeTab === "signIn" && "active"}`}
+                      data-toggle="tab"
+                      //   href="#signIn"
+                      role="tab"
+                      onClick={() => this.handleActiveTabChange("signIn")}
+                    >
+                      Sign In
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class={`nav-link ${activeTab === "createAccount" &&
+                        "active"}`}
+                      data-toggle="tab"
+                      //   href="#createAccount"
+                      role="tab"
+                      onClick={() =>
+                        this.handleActiveTabChange("createAccount")
+                      }
+                    >
+                      Sign Up
+                    </a>
+                  </li>
+                </ul>
 
-        if (localStorage.getItem('serviceproviderid') != null) {
-            
-        }
+                <div class="tab-content mb-5">
+                  <div
+                    className={
+                      "tab-pane fade in " +
+                      (activeTab === "signIn" && "show active")
+                    }
+                    // id="signIn"
+                    role="tabpanel"
+                  >
+                    {activeTab === "signIn" && <AuthenticateServiceProvider />}
+                  </div>
 
-        return (
-
-            <div id="MainPageWrapper">
-                {this.renderRedirect()}
-                <section className="section-padding">
-                    <div className="container">
-                        <div className="row pb-4 mb-5 pt-5">
-                            <div className="col-md-12 signinRegisterWrap mt-5 mb-5">
-
-                                <ul class="nav nav-tabs nav-justified signinRegister" role="tablist">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" data-toggle="tab" href="#signIn" role="tab">
-                                            Sign In
-                                      </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" data-toggle="tab" href="#createAccount" role="tab">
-                                            Sign Up
-                                      </a>
-                                    </li>
-                                </ul>
-
-                                <div class="tab-content mb-5">
-                                    <div class="tab-pane fade in show active" id="signIn" role="tabpanel">
-                                        <AuthenticateServiceProvider />
-                                    </div>
-
-                                    <div class="tab-pane fade in" id="createAccount" role="tabpanel">
-                                        <RegisterServiceProvider />
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                </section>
+                  <div
+                    className={
+                      "tab-pane fade in " +
+                      (activeTab === "createAccount" && "show active")
+                    }
+                    // id="createAccount"
+                    role="tabpanel"
+                  >
+                    {activeTab === "createAccount" && (
+                      <RegisterServiceProvider
+                        onFinish={this.handleActiveTabChange}
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
